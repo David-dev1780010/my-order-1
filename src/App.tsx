caff09a7860373f417c7d02b901af7776197dc35
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-25 10:14:25 +0300

    Добавлено фоновое изображение с прозрачностью 30%

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import fon from './assets/images/fon.png';
 
 const App: React.FC = () => {
   return (
@@ -6,12 +7,30 @@ const App: React.FC = () => {
       width: '100%',
       height: '100%',
       backgroundColor: '#150B2C',
+      position: 'relative',
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       fontSize: '24px',
     }}>
-      mini app
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundImage: `url(${fon})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        opacity: 0.3,
+        zIndex: 1,
+      }} />
+      <div style={{
+        position: 'relative',
+        zIndex: 2,
+      }}>
+        mini app
+      </div>
     </div>
   );
 };
