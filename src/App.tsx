441d84504e65537e2ec17c1e3f9c09dc8477de93
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-25 09:53:41 +0300

    Добавлен фон на весь экран и глобальные стили

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div style={{
-      width: '100vw',
-      height: '100vh',
+      width: '100%',
+      height: '100%',
       backgroundColor: '#150B2C',
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-      color: 'white',
       fontSize: '24px',
-      fontFamily: 'Arial, sans-serif'
     }}>
       mini app
     </div>
