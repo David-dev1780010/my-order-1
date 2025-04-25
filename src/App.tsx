10159e775f5b5edb0964fb8ecca955d432254dc6
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-25 10:19:01 +0300

    Изменено позиционирование фонового изображения

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App: React.FC = () => {
         bottom: 0,
         backgroundImage: 'url(/images/fon.png)',
         backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        backgroundPosition: 'top',
         opacity: 0.3,
         zIndex: 1,
       }} />
